adde999ebd4f1d467331a957bf9f611cbf3a92dd
NathanaelV <tmntsplinter04@gmail.com>
2022-01-26 11:31:21 -0300

    Auteração para funcionar o get 'locacoes' to: 'home#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'locacoes', to: 'home#index'
+  #get 'carros', to: 'home#carros'
 end
+
