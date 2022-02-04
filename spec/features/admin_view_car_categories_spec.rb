e379e6b1b64a8166233e1b2d39b450f55e56c285
NathanaelV <tmntsplinter04@gmail.com>
2022-02-04 06:48:27 -0300

    Completando os testes do admin_view_car_categories

--- spec/features/admin_view_car_categories_spec.rb
+++ spec/features/admin_view_car_categories_spec.rb
@@ -32,14 +32,33 @@ feature 'Admin view car categories' do
   end
 
   scenario 'and no car categories are created' do
+    visit root_path
+    click_on 'Categorias'
+
+    expect(page).to have_content('Nenhuma categoria cadastrada')
   end
 
   scenario 'and return to home page' do
+    CarCategory.create!(name: 'Top', daily_rate: 105.5, car_insurance: 58.5
+                        third_party_insurance: 10.5)
     
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Voltar'
+
+    expect(current_path).to eq root_path
   end
 
   scenario 'and return to manufacturers page' do
+    CarCategory.create!(name: 'Top', daily_rate: 105.5, car_insurance: 58.5
+                        third_party_insurance: 10.5)
     
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Top'
+    click_on 'Voltar'
+
+    expect(current_path).to eq car_categories_path
   end
 end
 
