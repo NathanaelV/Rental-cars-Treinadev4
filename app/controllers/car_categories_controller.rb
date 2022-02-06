0a8230052d711a0c676dab5150dc3429c165f3f8
NathanaelV <tmntsplinter04@gmail.com>
2022-02-06 07:40:39 -0300

    Começando o and view details do teste admin_view_car_categories

--- app/controllers/car_categories_controller.rb
+++ app/controllers/car_categories_controller.rb
@@ -2,5 +2,9 @@ class CarCategoriesController < ApplicationController
   def index
     @car_categories = CarCategory.all
   end
+
+  def show
+    @car_category = CarCategory.find(params[:id]) # params é um HASH
+  end
 end
 
