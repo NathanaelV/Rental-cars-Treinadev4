a79a2d8c80d1eb1f61d067a617e32e627d197ab9
NathanaelV <tmntsplinter04@gmail.com>
2022-03-01 13:19:52 -0300

    Apagando categoria de carros

---
+++ spec/features/car_category/admin_deletes_car_category_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+feature 'Admin deletes car category' do
+  scenario 'successfully' do
+    CarCategory.create!(name: 'Top', daily_rate: 105.5, car_insurance: 58.5, 
+                        third_party_insurance: 10.5)
+
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Top'
+    click_on 'Apagar'
+
+    expect(current_path).to eq car_categories_path
+    expect(page).to have_content('Nenhuma categoria cadastrada')
+  end
+
+  scenario 'and keep anothers' do
+    CarCategory.create!(name: 'Top', daily_rate: 105.5, car_insurance: 58.5, 
+                        third_party_insurance: 10.5)
+    CarCategory.create!(name: 'Flex', daily_rate: 80, car_insurance: 8.5, 
+                        third_party_insurance: 8.5)
+
+    visit root_path
+    click_on 'Categorias'
+    click_on 'Top'
+    click_on 'Apagar'
+
+    expect(current_path).to eq car_categories_path
+    expect(page).not_to have_content('Top')
+    expect(page).to have_content('Flex')
+  end  
+end
+
+
+=begin
+  
+scenario 'Must be signed in' do
+    # Arrange
+
+    # Act
+    visit root_path
+    click_on 'Categorias'
+
+    # Assert
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content('Para continuar, faça login ou registre-se.')
+  end
+
+  scenario 'and sign out' do
+    # Arrange
+    user = User.create!(name: 'Rapha', email: 'rapha@email.com', password: '123456')
+    login_as(user, scope: :user)
+
+    # Act
+    visit root_path
+    click_on 'Sair'
+
+    # Assert
+    expect(page).not_to have_content(user.name)
+    expect(page).not_to have_content('Sair')
+    expect(page).to have_content('Logout efetuado com sucesso')
+    expect(page).to have_content('Entrar')
+  end
+  
+=end
